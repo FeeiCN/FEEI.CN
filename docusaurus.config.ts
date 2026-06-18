c707c8eceb09a3817161dd7de9c574af7fbbe938
Feei <feei@feei.cn>
2026-06-18 19:50:22 +0800

    docusaurus.config: 切换部署域名为 feei.cn

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -120,7 +120,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://wufeifei.com',
+  url: 'https://feei.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -128,7 +128,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'feeicn', // Usually your GitHub org/user name.
-  projectName: 'wufeifei.com', // Usually your repo name.
+  projectName: 'FEEI.CN', // Usually your repo name.
 
   onBrokenLinks: isStrictBuild ? 'throw' : 'warn',
 
